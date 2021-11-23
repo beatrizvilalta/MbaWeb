7adb4edcffe5a04549dc92afb40ffcf3e0b83cf9
Gabe <gabrielrosadn@gmail.com>
2021-11-23 15:36:09 -0300

    Add payment form screen

--- src/views/PaymentForm.vue
+++ src/views/PaymentForm.vue
@@ -1,27 +1,46 @@
 <template>
-    <div>
-        <h1>This is {{title}}</h1>
-        <p> 
-            Atributos:  <br>             
-            value - double <br> 
-            relative_date - date <br> 
-            observation - string <br>
-        
-        </p>
+<div class="hero">
+    <Navbar> </Navbar>
+    <div class="hero-body p-5">
+        <p class= "is-size-3"> {{ title }} </p>
+        <div class="columns is-centered">
+            <div class="box mt-5">
+                <div class="column">
+                    <form action="" method="post">
+                        <div class="field is-horizontal columns pb-4">
+                            <label class="has-text-weight-semibold flex-center mr-4" for="reference-date">Mês de referência: </label>
+                            <date-picker v-model="referenceDate" type="month"></date-picker>
+                        </div>
 
-        <form action="/action_page.php">
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname" value="John"><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname" value="Doe"><br><br>
-        <input type="submit" value="Submit">
-        </form> 
+                        <div class="field is-horizontal columns pb-2">
+                            <label class="has-text-weight-semibold flex-center mr-4" for="payment-value">Valor: </label>
+                            <vue-numeric currency="R$" separator="." v-bind:precision="2" v-model="paymentValue" class="input"></vue-numeric>
+                        </div>
+
+                        <div class="field">
+                            <textarea v-model="observation" name="observation" id="observation" rows="5" class="textarea is-medium" placeholder="Observações"></textarea>
+                        </div>
+
+                        <button type="submit" class="button is-success is-size-5">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    
+</div>
 </template>
-
 <script>
+import DatePicker from 'vue2-datepicker';
+import Navbar from '../components/Navbar.vue'
+import VueNumeric from 'vue-numeric'
+import 'vue2-datepicker/index.css';
+
 export default {
+    components: {
+        DatePicker,
+        Navbar,
+        VueNumeric
+    },
     props: {
         paymentId: Number
     },
@@ -33,17 +52,28 @@ export default {
     },
     data() {
         return {
-            title: ""
+            title: "",
+            referenceDate: new Date(),
+            paymentValue: 0,
+            observation: ""
         };
     },
     methods: {    
         setupView() {
+            // this.$session.get("userid")
             if (this.$route.params.paymentId == null) {
-                this.title = "Add payment for user " + this.$session.get("userid")
+                this.title = "Adicionar pagamento"
             } else {
-                this.title = "Edit payment for user " + this.$session.get("userid") + " And paymentId id " + this.$route.params.paymentId
+                this.title = "Editar pagamento"
             }
         }    
     }
 }
 </script>
+
+<style>
+.flex-center {
+    display: flex;
+    align-items: center;
+}
+</style>
